4be975778dc472ff6eab5eb7f438b728c0a78f96
Lorna <lornacr2@hotmail.com>
2025-04-27 19:25:51 -0400

    fixed contract

--- contracts/ArtNFT.sol
+++ contracts/ArtNFT.sol
@@ -8,50 +8,48 @@ contract ArtNFT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
     mapping(uint256 => uint256) public prices;
     mapping(address => uint256) public earnings;
+    mapping(uint256 => bool) public purchased; // Track if the NFT has been purchased
 
     constructor() ERC721("ArtNFT", "ART") Ownable(msg.sender) {}
 
     event ArtMinted(address indexed creator, uint256 tokenId, string tokenURI);
     event ArtSold(address indexed buyer, uint256 tokenId, uint256 price);
 
-    function mint(string memory tokenUri) external returns (uint256) {
+    // Mint the NFT and list it for sale immediately
+    function mintAndListForSale(string memory tokenUri, uint256 price) external returns (uint256) {
         uint256 tokenId = nextTokenId;
+        
+        // Mint the NFT
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenUri);
         nextTokenId++;
+
+        // List the NFT for sale
+        prices[tokenId] = price;
+        
+        // Emit events
         emit ArtMinted(msg.sender, tokenId, tokenUri);
+
         return tokenId;
     }
 
+    // Function to list the NFT for sale (this is optional now as it's handled by mintAndListForSale)
     function listForSale(uint256 tokenId, uint256 price) external {
         require(ownerOf(tokenId) == msg.sender, "Not your token");
         prices[tokenId] = price;
     }
 
-    function buy(uint256 tokenId) external payable {
-        uint256 price = prices[tokenId];
-        address seller = ownerOf(tokenId);
-        require(price > 0, "Not for sale");
-        require(msg.value >= price, "Insufficient ETH");
-
-        _transfer(seller, msg.sender, tokenId);
-        prices[tokenId] = 0;
-        earnings[seller] += msg.value;
-
-        emit ArtSold(msg.sender, tokenId, price);
-    }
-
-    //new buy function:
+    // Buy the NFT from the owner
     function buy(uint256 tokenId) external payable {
         uint256 price = prices[tokenId];
         address seller = ownerOf(tokenId);
         
         require(price > 0, "Not for sale");
         require(msg.value >= price, "Insufficient ETH");
-        
+
         // Transfer the NFT to the buyer
         _transfer(seller, msg.sender, tokenId);
-        
+
         // Mark the price as zero (no longer for sale)
         prices[tokenId] = 0;
 
@@ -65,6 +63,7 @@ contract ArtNFT is ERC721URIStorage, Ownable {
         emit ArtSold(msg.sender, tokenId, price);
     }
 
+    // Withdraw earnings from sales
     function withdraw() external {
         uint256 amount = earnings[msg.sender];
         require(amount > 0, "No earnings");
@@ -72,6 +71,7 @@ contract ArtNFT is ERC721URIStorage, Ownable {
         payable(msg.sender).transfer(amount);
     }
 
+    // Get total supply of minted NFTs
     function totalSupply() external view returns (uint256) {
         return nextTokenId;
     }
